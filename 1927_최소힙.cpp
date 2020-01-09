3dc0b7d58d2572aaf52e3c770163e7089394f02d
AnneMayor <melllamodahye@gmail.com>
2020-01-09 21:17:19 +0900

    1927_최소힙_백준

---
+++ 1927_최소힙.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+priority_queue<int, vector<int>, greater<int> > pq;
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        int cmd;
+        scanf("%d", &cmd);
+
+        if(!cmd) {
+            if(pq.empty()) {
+                printf("0\n");
+            } else {
+                printf("%d\n", pq.top());
+                pq.pop();
+            }
+        } else {
+            pq.push(cmd);
+        }
+    }
+
+    return 0;
+}
