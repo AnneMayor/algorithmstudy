98eab48c1fd5aa21154cec99c1938b68be79c202
AnneMayor <melllamodahye@gmail.com>
2020-01-09 22:19:19 +0900

    2004_조합0의개수_백준 여기서 주목! num! 수의 2나 5의 개수 = 숫자 num / 2 or 5 int 범위...for문에서도 확인하자!

---
+++ 2004_조합0의개수.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int getFive(int num) {
+
+    int ret = 0;
+
+    for(long long i = 5; num / i >= 1; i *= 5) {
+        ret += num / i;
+    }
+
+    return ret;
+}
+
+int getTwo(int num) {
+
+    int ret = 0;
+
+    for(long long i = 2; num / i >= 1; i *= 2) {
+        ret += num / i;
+    }
+
+    return ret;
+}
+
+int main() {
+
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    int fiveAns = 0, twoAns = 0;
+
+    int fiveN = getFive(n);
+    int fiveM = getFive(m);
+    int fiveNM = getFive(n-m);
+
+    int twoN = getTwo(n);
+    int twoM = getTwo(m);
+    int twoNM = getTwo(n-m);
+
+    fiveAns = fiveN - (fiveM + fiveNM);
+    twoAns = twoN - (twoM + twoNM);
+
+    if(fiveAns < 0 || twoAns < 0) {
+        printf("0\n");
+    } else {
+        printf("%d\n", min(fiveAns, twoAns));
+    }
+
+    return 0;
+}
