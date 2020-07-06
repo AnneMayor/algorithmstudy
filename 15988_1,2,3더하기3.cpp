29658698562a99b1619cf1bd930a4c20241b0a5e
Dahye Lee <dahyeannelee@MacBook-Pro.local>
2020-07-06 22:42:39 +0900

    15988_1,2,3더하기3_백준 좀 더 빠르게 실행하는 코드!

--- 15988_1,2,3더하기3.cpp
+++ 15988_1,2,3더하기3.cpp
@@ -2,11 +2,12 @@
 
 using namespace std;
 
-const int MAX = 1e6+5;
+const int MAX = 1e6 + 5;
 
 long long numOfCases[MAX];
 
-int main() {
+int main()
+{
 
     int T;
     scanf("%d", &T);
@@ -15,12 +16,15 @@ int main() {
     numOfCases[2] = 2;
     numOfCases[3] = 4;
 
-    for(int tc = 1; tc <= T; tc++) {
+    for (int i = 4; i <= 1e6+1; i++)
+    {
+        numOfCases[i] = (numOfCases[i - 1] + numOfCases[i - 2] + numOfCases[i - 3]) % 1000000009;
+    }
+
+    for (int tc = 1; tc <= T; tc++)
+    {
         int n;
         scanf("%d", &n);
-        for(int i = 4; i <= n; i++) {
-            numOfCases[i] = (numOfCases[i-1] + numOfCases[i-2] + numOfCases[i-3])%1000000009;
-        }
 
         printf("%lld\n", numOfCases[n]);
     }
