c99fd4b552998e9f9a9a0d6c86a5472191d65c82
Dahye Lee <melllamodahye@gmail.com>
2020-10-10 17:29:23 +0900

    프로그래머스_월간코드챌린지_2020_풍선터뜨리기 음,,,논리는 접근했으나 이걸 코드로 어떻게 찾을지를 고민함,,,좀 더 연습하자!

--- Programmers_월간코드챌린지_2020_No3.cpp
+++ Programmers_월간코드챌린지_2020_No3.cpp
@@ -1,77 +1,36 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-const int MAX = 1005;
+const int MAX = 1e7;
 
-int triNums[MAX][MAX];
+int left_minimum[MAX];
+int right_minimum[MAX];
 
-vector<int> solution(int n) {
-    int num = 1;
-    int sr = 0, er = n-1, sc = 0, ec = n-1;
-    int total = n*(n+1) / 2;
-
-    cout << "total: " << total << endl;
-    while (num <= total)
-    {
-        // 1. 아래
-        for(int i = sr; i <= er; i++) {
-            if(num > total) break;
-            triNums[i][sc] = num++;
-        }
-        sr++;
-        sc++;
-
-        // 2. 오른쪽
-        for(int i = sc; i <= ec; i++) {
-            if(num > total) break;
-            triNums[er][i] = num++;
-        }
-        ec--;
-        er--;
-
-        // 3. 대각선 위
-        int tmpX = ec, tmpY = ec;
-        for(int i = er; i >= sr; i--) {
-            if(num > total) break;
-            triNums[i][tmpY--] = num++;
-        }
-        sr++;
-        ec--;
+int solution(vector<int> a) {
+    int answer = 0;
+    // 1. 좌에서 우로 각 배열 index별 최소값 구하기
+    int mn = INT_MAX;
+    int idx = 0;
+    for(auto num : a) {
+        if(mn > num) mn = num;
+        left_minimum[idx++] = mn;
     }
 
-    // for(int i = 0; i < n; i++) {
-    //     for(int j = 0; j < n; j++) {
-    //         cout << triNums[i][j] << " ";
-    //     }
-    //     cout << endl;
-    // }
-    
-    vector<int> answer;
+    // 2. 우에서 좌로 각 배열 index별 최소값 구하기
+    idx = (int)a.size()-1;
+    mn = INT_MAX;
+    for(int i = (int)a.size()-1; i >= 0; i--) {
+        if(mn > a[i]) mn = a[i];
+        right_minimum[i] = mn;
+    }
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            if(!triNums[i][j]) answer.push_back(triNums[i][j]);
-        }
+    // 3. 각 배열별 풍선 터지는지 유무 판단
+    idx = 0;
+    for(auto num : a) {
+        int mx = max(left_minimum[idx], right_minimum[idx]);
+        if(num <= mx) answer++;
+        idx++;
     }
 
     return answer;
-}
-
-int main(void) {
-
-    memset(triNums, 0, sizeof(triNums));
-
-    solution(1);
-    cout << "-------" << endl;
-    solution(2);
-    cout << "-------" << endl;
-    solution(3);
-    cout << "-------" << endl;
-    solution(4);
-    cout << "-------" << endl;
-    solution(5);
-    cout << "-------" << endl;
-    solution(6);
-
-    return 0;
 }
