7f2724e49837943b3e8988b091ceb12eb609a1ad
Dahye Lee <melllamodahye@gmail.com>
2021-03-02 20:27:08 +0900

    프로그래머스_스택,큐 기능개발 최적풀이 & 나의 풀이 2가지

---
+++ Programmers_stack_기능개발.cpp
@@ -0,0 +1,52 @@
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    
+    vector<int> days;
+    int max_day = 0;
+
+    for(int idx = 0; idx < progresses.size(); idx++) {
+        int day = (100 - progresses[idx]) % speeds[idx] == 0 ? (100-progresses[idx]) / speeds[idx] : (100-progresses[idx]) / speeds[idx] + 1;
+
+        if(days.empty() || max_day < day) {
+            max_day = day;
+            days.push_back(1);
+        } else {
+            days.back()++;
+        }
+    }
+
+    for(int idx = 0; idx < progresses.size(); idx++) {
+        int remain = 100 - progresses[idx];
+        int workDay = remain % speeds[idx] == 0 ? remain / speeds[idx] : remain / speeds[idx] + 1;
+        days.push_back(workDay);
+    }
+    
+    stack<int> order;
+    vector<int> answer;
+    int startIdx = 0;
+    for(auto day : days) {
+        if(order.empty()) {
+            order.push(day);
+            answer.push_back(1);
+        } else {
+            int releaseDay = order.top();
+            order.pop();
+            if(releaseDay < day) {
+                startIdx++;
+                answer.push_back(1);
+                order.push(day);
+            } else {
+                answer[startIdx] += 1;
+                order.push(releaseDay);
+            }
+        }
+    }
+    
+    return answer;
+    // return days;
+}
