521ed8cb4783ce21e9dd98f2b99bf57cf264fc37
Dahye Lee <melllamodahye@gmail.com>
2021-03-03 18:58:42 +0900

    시간복잡도는 이전과 동일하나 로직 부분에서 개선한 코드

--- Programmers_stack_기능개발.cpp
+++ Programmers_stack_기능개발.cpp
@@ -9,22 +9,23 @@ vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> days;
     int max_day = 0;
 
-    for(int idx = 0; idx < progresses.size(); idx++) {
-        int day = (100 - progresses[idx]) % speeds[idx] == 0 ? (100-progresses[idx]) / speeds[idx] : (100-progresses[idx]) / speeds[idx] + 1;
+    // 다른 사람 풀이 중 가장 최적화(시간, 공간)된 풀이
+    // for(int idx = 0; idx < progresses.size(); idx++) {
+    //     int day = (100 - progresses[idx]) % speeds[idx] == 0 ? (100-progresses[idx]) / speeds[idx] : (100-progresses[idx]) / speeds[idx] + 1;
 
-        if(days.empty() || max_day < day) {
-            max_day = day;
-            days.push_back(1);
-        } else {
-            days.back()++;
-        }
-    }
+    //     if(days.empty() || max_day < day) {
+    //         max_day = day;
+    //         days.push_back(1);
+    //     } else {
+    //         days.back()++;
+    //     }
+    // }
 
-    for(int idx = 0; idx < progresses.size(); idx++) {
-        int remain = 100 - progresses[idx];
-        int workDay = remain % speeds[idx] == 0 ? remain / speeds[idx] : remain / speeds[idx] + 1;
-        days.push_back(workDay);
-    }
+    // for(int idx = 0; idx < progresses.size(); idx++) {
+    //     int remain = 100 - progresses[idx];
+    //     int workDay = remain % speeds[idx] == 0 ? remain / speeds[idx] : remain / speeds[idx] + 1;
+    //     days.push_back(workDay);
+    // }
     
     stack<int> order;
     vector<int> answer;
