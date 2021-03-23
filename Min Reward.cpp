3398820351ccb785abdef1e9ba0e8827f50dbaf4
Dahye Lee <melllamodahye@gmail.com>
2021-03-23 18:40:10 +0900

    AlgoExpert(Hard): Min Reward O(n) Time 최적화 시키는 알고리즘 풀이...일반적인 풀이는 O(n^2)

---
+++ Min Reward.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int minRewards(vector<int> scores) {
+  int totalMinRewards = 0;
+
+  int n = scores.size();
+  vector<int> rewards(n, 1);
+
+  // 1. Compare each score from left to right
+  for(int i = 0; i < n-1; i++) {
+    if(scores[i] < scores[i+1]) {
+      rewards[i+1] = max(rewards[i+1], rewards[i]+1);
+    }
+  }
+
+  // 2. Compare each score from right to left
+  for(int i = n-1; i >= 1; i--) {
+    if(scores[i] < scores[i-1]) {
+      rewards[i-1] = max(rewards[i-1], rewards[i] + 1);
+    }
+  }
+
+  for(int i = 0; i < n; i++) {
+    totalMinRewards += rewards[i];
+  }
+
+  return totalMinRewards;
+}
