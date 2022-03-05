5c6ca032ae8c2a96427a49421da96004a8f2699e
Dahye Lee <melllamodahye@gmail.com>
2022-03-05 21:28:13 +0900

    Advanced 컬럼명 수정

--- HackerRank_Day2.sql
+++ HackerRank_Day2.sql
@@ -43,7 +43,7 @@ WHERE salary > 2000 AND months < 10
 ORDER BY employee_id;
 
 -- Advanced Quary: Hierarchy SQL --
-SELECT C.company_code, C.founder, count(DISTINCT lead_manager_code), count(DISTINCT senior_manager_code), count(DISTINCT managers_code), count(DISTINCT employee_code)
+SELECT C.company_code, C.founder, count(DISTINCT lead_manager_code), count(DISTINCT senior_manager_code), count(DISTINCT manager_code), count(DISTINCT employee_code)
 FROM Company C INNER JOIN Employee E ON C.company_code = E.company_code
 GROUP BY C.company_code, C.founder
 ORDER BY C.company_code;
