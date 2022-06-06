7193a41bd59237638f1ab9e66f538b02f81c5fd6
AnneMayor <melllamodahye@gmail.com>
2022-06-06 18:10:01 +0900

    LeetCode Daily Practice

---
+++ LeetCode_getIntersectionNode.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class LeetCode_getIntersectionNode {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode nodeA = headA, nodeB = headB;
+        
+        while(nodeA != nodeB) {
+            nodeA = nodeA == null ? headB : nodeA.next;
+            nodeB = nodeB == null ? headA : nodeB.next;
+        }
+
+        return nodeA;
+    }
+}
