809ac382744cb1267cde07f39ab40d5363e738ed
AnneMayor <melllamodahye@gmail.com>
2022-09-04 23:18:32 +0900

    class명 중복 이슈 해결

--- 10025_게으른백곰.java
+++ 10025_게으른백곰.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Solution {
+public class 10025_게으른백곰 {
 
     public static void main(String...args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
