a2ea7aec7963aa7ac122e9a74e06fe66f8ca85fd
AnneMayor <melllamodahye@gmail.com>
2022-09-26 05:58:25 +0900

    클래스명 변경

--- 10844_쉬운계단수.java
+++ 10844_쉬운계단수.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class 10844_쉬운계단수 {
 
     public static void main(String...args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
