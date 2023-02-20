d5065312ede220c5164d9f3594436a6718cce3e1
AnneMayor <melllamodahye@gmail.com>
2023-02-21 08:32:17 +0900

    LeetCode 풀이 추가

--- LeetCode_FindTheTownJudge.java
+++ LeetCode_FindTheTownJudge.java
@@ -1,19 +1,32 @@
 public class Solution {
     public int findJudge(int n, int[][] trust) {
-        Map<Integer, Integer> trusts = new HashMap<>();
-        boolean[] trusting = new boolean[n + 1];
+//        Map<Integer, Integer> trusts = new HashMap<>();
+//        boolean[] trusting = new boolean[n + 1];
+//
+//        if (n == 1) return n;
+//
+//        for (int [] t : trust) {
+//            trusting[t[0]] = true;
+//            trusts.put(t[1], trusts.getOrDefault(t[1], 0) + 1);
+//        }
+//
+//        for (int i = 1; i <= n; i++) {
+//            if (trusts.get(i) != null && trusts.get(i) == n - 1 && !trusting[i]) {
+//                return i;
+//            }
+//        }
+//
+//        return -1;
 
-        if (n == 1) return n;
+        int [] count = new int[n+1];
 
-        for (int [] t : trust) {
-            trusting[t[0]] = true;
-            trusts.put(t[1], trusts.getOrDefault(t[1], 0) + 1);
+        for (int[] t : trust) {
+            count[t[0]]--;
+            count[t[1]]++;
         }
 
         for (int i = 1; i <= n; i++) {
-            if (trusts.get(i) != null && trusts.get(i) == n - 1 && !trusting[i]) {
-                return i;
-            }
+            if (count[i] == n-1) return i;
         }
 
         return -1;
